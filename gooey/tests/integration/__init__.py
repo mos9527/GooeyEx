4e219ca8958509add2d774b4dcad3733b1886320
mos9527 <greats3an@gmail.com>
2025-04-09 16:50:24 +0800

    0.0.1

--- gooey/tests/integration/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Integration tests that exercise Gooey's various run modes
-
-WX Python needs to control the main thread. So, in order to simulate a user
-running through the system, we have to execute the actual assertions in a
-different thread
-"""
--- setup.py
+++ setup.py
@@ -1,74 +1,50 @@
 """Script for setuptools."""
-import sys
+
+import glob
 from setuptools import setup, find_packages
 
 
-with open('README.md') as readme:
+with open("README.md") as readme:
     long_description = readme.read()
 
-version = '1.2.0-ALPHA'
+version = "0.0.1"
 
 deps = [
-    'Pillow>=4.3.0',
-    'psutil>=5.4.2',
-    'colored>=1.3.93',
-    'pygtrie>=2.3.3',
-    're-wx>=0.0.9',
-    'typing-extensions==3.10.0.2',
-    'wxpython>=4.1.0',
-    'dataclasses>=0.8; python_version < "3.7"'
+    "Pillow>=4.3.0",
+    "psutil>=5.4.2",
+    "colored>=1.3.93",
+    "pygtrie>=2.3.3",
+    "re-wx>=0.0.9",
+    "typing-extensions==3.10.0.2",
+    "wxpython>=4.1.0",
+    'dataclasses>=0.8; python_version < "3.7"',
 ]
 
 setup(
-    name='Gooey',
+    name="GooeyEx",
     version=version,
-    url='http://pypi.python.org/pypi/Gooey/',
-    author='Chris Kiehl',
-    author_email='audionautic@gmail.com',
-    description=('Turn (almost) any command line program into a full GUI '
-                 'application with one line'),
-    license='MIT',
-    python_requires='>=3.6',
+    url="http://pypi.python.org/pypi/GooeyEx/",
+    author="greats3an",
+    author_email="greats3an@gmail.com",
+    description=(
+        "GooeyEx is a fork of Gooey, a Python library that turns command line programs into GUI applications."
+    ),
+    license="MIT",
+    python_requires=">=3.6",
     packages=find_packages(),
     install_requires=deps,
     include_package_data=True,
-    dependency_links = ["http://www.wxpython.org/download.php"],
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Desktop Environment',
-        'Topic :: Software Development :: Build Tools',
-        'Topic :: Software Development :: Widget Sets',
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License'
+    dependency_links=["http://www.wxpython.org/download.php"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Desktop Environment",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Widget Sets",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
     ],
-    long_description='''
-
-Gooey (Beta)
-############
-
-
-Turn (almost) any Python Console Program into a GUI application with one line
------------------------------------------------------------------------------
-
-.. image:: https://cloud.githubusercontent.com/assets/1408720/7904381/f54f97f6-07c5-11e5-9bcb-c3c102920769.png
-
-
-Quick Start
------------
-
-Gooey is attached to your code via a simple decorator on your `main` method.
-
-.. code-block::
-
-  from gooey import Gooey
-  @Gooey      <--- all it takes! :)
-  def main():
-      # rest of code
-
-
-
-With the decorator attached, run your program and the GUI will now appear!
-
-Checkout the full documentation, instructions, and source on `Github <https://github.com/chriskiehl/Gooey>`_'''
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    data_files=glob.glob("GooeyEx/languages/*") + glob.glob("GooeyEx/images/*"),
 )
